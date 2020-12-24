4583d7e81594ddb32c1e9211fec662f0c264f191
Ethan Olsen <46045126+o2dependent@users.noreply.github.com>
2020-12-24 02:32:29 -0800

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const { default: colors } = require("./src/styles/colors")
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -18,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `TicTacToe`,
+        short_name: `TicTacToe`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: colors.dark,
+        theme_color: colors.dark,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tictactoe.png`, // This path is relative to the root of the site.
       },
     },
     {
